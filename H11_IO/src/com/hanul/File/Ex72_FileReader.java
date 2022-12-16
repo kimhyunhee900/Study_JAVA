8fa9176e6277f3fb30a45d66ea4d627f899114b6
kimhyunhee900 <khh4477@naver.com>
2022-12-16 11:11:25 +0900

    72수정

--- H11_IO/src/com/hanul/File/Ex72_FileReader.java
+++ H11_IO/src/com/hanul/File/Ex72_FileReader.java
@@ -2,24 +2,59 @@ package com.hanul.File;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Ex72_FileReader {
 
-	public static void main(String[] args) {
-		//문자파일 읽고 쓰기
-		FileReader reader = null;
-		try {
-			reader = new FileReader("reader.txt");
-			int data;
-			while((data=reader.read()) != -1 ) {
-				char ch = (char)data;
-				System.out.print(ch);
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-			
-	}
+   public static void main(String[] args) {
+      // 문자 파일 읽고 쓰기
+      FileReader reader = null;
+      try {
+         reader = new FileReader("reader.txt");
+         while(true) {         
+            
+//               int data;
+//               while((data=reader.read()) != -1 ) {
+//                  char ch = (char) reader.read();
+//                  System.out.println(ch);
+//               }
+            int data = reader.read();
+            if(data == -1) {
+               break;
+            }
+            char ch = (char) reader.read();
+            System.out.println(ch);
+            
+                        
+         }
+         
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }finally {
+         try {
+            reader.close();            
+         } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
+      }
+      
+      try {
+         reader = new FileReader("reader.txt");
+         char[] data = new char[100];
+         
+         int no = reader.read(data);
+         System.out.println(no);
+         for(int idx=0; idx<no; idx++) {
+            System.out.println(data[idx]);
+         }
+         
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
+   }
+
 }
